c2d21fd6d04d2a2aa651212957e0c4ddccc0c47b
ShavitCohen <shavit.hit@gmail.com>
2017-11-05 18:48:49 +0200

    refactor(example1): refactoring code

--- example1/script.js
+++ example1/script.js
@@ -1,21 +1,21 @@
-(function () {
-
-    /*******************************************************
-     ******* Example 1 - Observable from array **********
-     *******************************************************/
-    const Observable = Rx.Observable;
-    const arr = [10, 30, null, 36, 70];
-
-    const arr$ = Observable.from(arr);
-
-    const observer = {
-        next: val => console.log(val),
-        error: err => console.log(err),
-        complete: () => console.log('completed')
-    }
-
-    arr$
-        .filter(x => !!x)
-        .subscribe(observer);
-
+(function (){
+	
+	/*******************************************************
+	 ******* Example 1 - Observable from array **********
+	 *******************************************************/
+	const Observable = Rx.Observable;
+	const arr = [10, 30, null, 36, 70];
+	
+	const arr$ = Observable.from(arr)
+	.filter(v => !!v)
+	.map(v => v * 10);
+	
+	const observer = {
+		next: val => console.log(val),
+		error: err => console.log(err),
+		complete: () => console.log('completed')
+	}
+	
+	arr$.subscribe(observer);
+	
 })();
