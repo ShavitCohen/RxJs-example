a5147309c8c24452870c400ae848c3f2aba9d764
ShavitCohen <shavit.hit@gmail.com>
2017-11-05 20:28:30 +0200

    refactor(example3): refactoring code

--- example3/script.js
+++ example3/script.js
@@ -4,10 +4,14 @@
      ******* Example 3 From mousemove events **********
      **************************************************/
     const Observable = Rx.Observable;
-
-    const container = document.getElementById('container'),
-        box = document.getElementById('box'),
-        unsubscribeButton = document.getElementById('unsubscribe-button');
+	
+	const container = document.querySelector('#container'),
+		box = container.querySelector('#box'),
+		unsubscribeButton = container.querySelector('#unsubscribe-button'),
+		subscribeButton = container.querySelector('#subscribe-button'),
+		subscribeButtonClick$ = Observable.fromEvent(subscribeButton, 'click'),
+		unsubscribeButtonClick$ = Observable.fromEvent(unsubscribeButton, 'click'),
+		events$ = Observable.fromEvent(container, 'mousemove');
 
 
 
