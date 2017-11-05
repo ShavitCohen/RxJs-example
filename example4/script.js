03a36e947374dffa1d32dfeb5036b771630e895b
ShavitCohen <shavit.hit@gmail.com>
2017-11-05 20:30:19 +0200

    refactor(example4): refactoring code

--- example4/script.js
+++ example4/script.js
@@ -5,7 +5,7 @@
 	 *******************************************************/
 	const Observable = Rx.Observable;
 	
-	let arr = `
+	const arr = `
 	Hi, What is your name?
 	Can you solve the maze ?
 	I don't know where I am all I know the first code is !@ffds#$%@
