7beb1f9dc2f91c4f88f941b51ac20da83c53e4f2
ShavitCohen <shavit.hit@gmail.com>
2017-11-05 20:30:03 +0200

    refactor(example4): refactoring code

--- example4/script.js
+++ example4/script.js
@@ -1,16 +1,15 @@
-(function () {
-
-    /***************************************?**********
-     ******* Example 4 Type like effect ***************
-     **************************************************/
-    const Observable = Rx.Observable;
-
-    let arr = '';
-    arr += 'Hi, What is your name?';
-    arr += '|';
-    arr += 'Can you solve the maze ?'
-    arr += '|';
-    arr += `I don't know where I am all I know the first code is !@ffds#$%@`;
-    
-
+(function (){
+	
+	/***************************************?**********
+	 ******* Example 4 Type like effect ***************
+	 **************************************************/
+	const Observable = Rx.Observable;
+	
+	const arr = `
+	Hi, What is your name?
+	Can you solve the maze ?
+	I don't know where I am all I know the first code is !@ffds#$%@
+	`;
+	
+	
 })();
